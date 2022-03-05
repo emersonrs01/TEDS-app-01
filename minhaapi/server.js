876c0dddb10a49f7eedf32ae385c4a83c35f7a93
emersonrs01 <emerson-rocha.2@hotmail.com.br>
2022-03-04 22:14:09 -0300

    primeira api

---
+++ minhaapi/server.js
@@ -0,0 +1,27 @@
+console.log("teste")
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const uri = "mongodb://admin:admin@localhost:27018/teste?authSource=teste";
+MongoClient.connect(uri,(err,client) =>{
+    if(err){
+        return console.log(err);
+    }
+    db = client.db('teste');
+    app.listen(3000,function (){
+        console.log("API rodando na porta 3000");
+        console.log("testar por http://localhost:3000");
+    })
+})
+
+app.get('/' , (req , res)=>{
+
+   res.send('Atendida a requisicao GET')
+
+})
