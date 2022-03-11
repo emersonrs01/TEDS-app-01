d267a9d2a9fc3ba3943c4222a282da3c1b984e40
emersonrs01 <emerson-rocha.2@hotmail.com.br>
2022-03-11 20:33:53 -0300

    Primeiro commit.

--- minhaapi/server.js
+++ minhaapi/server.js
@@ -1,4 +1,4 @@
-console.log("teste")
+console.log("Subindo API...")
 
 const express = require('express');
 
@@ -9,19 +9,23 @@ app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const uri = "mongodb://admin:admin@localhost:27018/teste?authSource=teste";
-MongoClient.connect(uri,(err,client) =>{
-    if(err){
+MongoClient.connect(uri, (err, client) => {
+    if (err) {
         return console.log(err);
     }
     db = client.db('teste');
-    app.listen(3000,function (){
+    app.listen(3000, function () {
         console.log("API rodando na porta 3000");
         console.log("testar por http://localhost:3000");
     })
 })
 
-app.get('/' , (req , res)=>{
+app.get('/', (req, res) => {
 
-   res.send('Atendida a requisicao GET')
+    res.send('Atendida a requisicao GET')
 
 })
+
+const animais = require('./animais');
+animais(app);
+
