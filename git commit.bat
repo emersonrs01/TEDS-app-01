dae69a45da25ff6c153f1cb98b981690230b65ef
emersonrs01 <emerson-rocha.2@hotmail.com.br>
2022-03-11 20:35:43 -0300

    Commit de 11/03/2022-20h35m43s

--- git commit.bat
+++ git commit.bat
@@ -1,3 +1,5 @@
 git add .
-git commit -m "Primeiro commit."
+set dataHora=%date:~0,2%/%date:~3,2%/%date:~6,4%-%time:~0,2%h%time:~3,2%m%time:~6,2%s
+git commit -m "Commit de %dataHora%"
 git push -u origin main
+timeout 5 
